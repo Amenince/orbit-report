c4941525dda9b4cc87e77049d1365e36113f555d
anthony_cook_ <amenince777@gmail.com>
2020-04-16 17:43:42 -0500

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'orbit-report';
+  title:string = 'orbit-report';
+
+  sourceList: Satellite[];
+  
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          // TODO: loop over satellites
+          
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          
+          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+ 
+       }.bind(this));
+    }.bind(this));
+  }
 }
+
