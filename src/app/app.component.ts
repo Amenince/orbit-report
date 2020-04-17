d254e1501f05ad1955fc8c9673c7d0cd1038f5fb
anthony_cook_ <amenince777@gmail.com>
2020-04-17 02:38:15 -0500

    finished code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   
   constructor() {
     this.sourceList = [];
-    this.displayList = [];
+   //  this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -26,11 +26,11 @@ export class AppComponent {
           for (let i = 0; i < fetchedSatellites.length; i++) {
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
-         };
-          this.displayList = this.sourceList.slice(0);
-       }.bind(this));
-    }.bind(this));
-  }
+         }
+         this.displayList = this.sourceList.slice(0);
+      }.bind(this));
+   }.bind(this));
+}
 
   search(searchTerm: string): void {
     
